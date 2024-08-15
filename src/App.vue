983c162ec0e2cb1dbf1e82bae310efcc42213b3a
ivana <ivanastrasorier@gmail.com>
2024-08-14 21:58:49 -0300

    creo estructura del menu sidebar y cambio iconos

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterView } from 'vue-router'
+
+import HeaderBar from './components/HeaderBar.vue'
+import Sidebar from '@/components/Sidebar.vue'
+import NavigationBar from './components/NavigationBar.vue'
+</script>
+
+<template>
+  <Sidebar />
+  <div class="w-100">
+    <div class="headers w-100">
+      <HeaderBar />
+      <div class="line"></div>
+      <NavigationBar />
+    </div>
+    <div class="container workarea">
+      <RouterView />
+    </div>
+  </div>
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,57 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import PlanificacionView from '@/views/PlanificacionView.vue'
+import DashboardView from '@/views/DashboardView.vue'
+import IncommingsView from '@/views/IncommingsView.vue'
+import OutgoingsView from '@/views/OutgoingsView.vue'
+import TrucksView from '@/views/TrucksView.vue'
+import DriversView from '@/views/DriversView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutView
+    },
+    {
+      path: '/planificacion',
+      name: 'planificacion',
+      component: PlanificacionView
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: DashboardView
+    },
+    {
+      path: '/incommings',
+      name: 'incommings',
+      component: IncommingsView
+    },
+    {
+      path: '/outgoings',
+      name: 'outgoings',
+      component: OutgoingsView
+    },
+    {
+      path: '/trucks',
+      name: 'trucks',
+      component: TrucksView
+    },
+    {
+      path: '/drivers',
+      name: 'drivers',
+      component: DriversView
+    }
+  ]
+})
+
+export default router
